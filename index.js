e990ad4182ad1e4f433db9851f11b3248d8268b0
marie-lisehamel <marie-lise.hamel@finastra.com>
2020-07-06 12:47:04 -0500

    add function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const { extname } = require('path');
 
 /* const server = http.createServer((req, res) => {
   if (req.url === '/') {
@@ -10,7 +11,7 @@ const fs = require('fs');
 }); */
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+  /*   if (req.url === '/') {
     fs.readFile(
       path.join(__dirname, 'public', 'index.html'),
       (err, content) => {
@@ -29,7 +30,47 @@ const server = http.createServer((req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(users));
+  } */
+
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  let extName = path.extname(filePath);
+
+  let contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.css':
+      contentType = 'test/css';
+      break;
   }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      console.log(err.code);
+      if (err.code == 'ENOENT') {
+        console.log('not found');
+        fs.readFile(
+          path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, 'text/html');
+            res.end(content, 'utf8');
+          }
+        );
+      }
+    } else {
+      res.writeHead(200, contentType);
+      res.end(content);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000; // check env variable or default to 5000
